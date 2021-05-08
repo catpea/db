7ecd17f6313bb2f4ae945d66e9196ea809d66615
catpea <corporate.miou@gmail.com>
2021-05-08 12:58:55 -0400

    New Database Revision

--- test.sh
+++ test.sh
@@ -1,78 +1,54 @@
 #!/usr/bin/env bash
 
+
 if test -f .INSTALLED; then
-  echo "[OK] system installed";
+  echo "$(tput setaf 2)[OK]$(tput sgr 0) system installed";
 else
-  echo "[NO] db does not appear to be installed";
-  echo "SOLUTION:";
-  echo "./install.sh";
-  exit;
+  echo "$(tput setaf 1)[NO]$(tput sgr 0) db does not appear to be installed. SOLUTION: ./install.sh";
 fi
 
 if command -v node &> /dev/null; then
-  echo "[OK] node is installed"
+  echo "$(tput setaf 2)[OK]$(tput sgr 0) node is installed"
 else
-  echo "[NO] node is not installed, "
-  echo "SOLUTION:";
-  echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash"
-  echo "nvm install node";
-  exit;
+  echo "$(tput setaf 1)[NO]$(tput sgr 0) node is not installed. SOLUTION: curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash; nvm install node";
 fi
 
 if command -v montage &> /dev/null; then
-  echo "[OK] ImageMagick found"
+  echo "$(tput setaf 2)[OK]$(tput sgr 0) ImageMagick found"
 else
-  echo "[NO] ImageMagick not found"
-  echo "SOLUTION:";
-  echo "apt get imagemagick"
-  echo "dnf install imagemagick"
-  exit;
+  echo "$(tput setaf 1)[NO]$(tput sgr 0) ImageMagick not found. SOLUTION: install imagemagick"
 fi
 
 if command -v ffmpeg &> /dev/null; then
-  echo "[OK] FFMPEG found"
+  echo "$(tput setaf 2)[OK]$(tput sgr 0) FFMPEG found"
 else
-  echo "[NO] FFMPEG not found"
-  echo "SOLUTION:";
-  echo "apt get ffmpeg"
-  echo "dnf install ffmpeg"
-  exit;
+  echo "$(tput setaf 1)[NO]$(tput sgr 0) FFMPEG not found. SOLUTION: install ffmpeg"
 fi
 
 if command -v eternia &> /dev/null; then
-  echo "[OK] eternia is installed"
+  echo "$(tput setaf 2)[OK]$(tput sgr 0) eternia is installed"
 else
-  echo "[NO] eternia not found"
-  echo "SOLUTION:";
-  echo "npm -g i eternia"
-  exit;
+  echo "$(tput setaf 1)[NO]$(tput sgr 0) eternia not found. SOLUTION: npm -g i eternia"
 fi
 
 if [[ -z $(cd bin; git status -s) ]]; then
-    echo "[OK] bin is in sync"
+    echo "$(tput setaf 2)[OK]$(tput sgr 0) bin is in sync"
 else
-    echo "[NO] bin has uncommited changes"
-    echo "SOLUTION:";
-    echo "./system-publish.sh"
-    exit;
+    echo "$(tput setaf 1)[NO]$(tput sgr 0) bin has uncommited changes. SOLUTION: ./system-publish.sh"
 fi
 
 if [[ -z $(git status -s) ]]; then
-    echo "[OK] db is in sync"
+    echo "$(tput setaf 2)[OK]$(tput sgr 0) db is in sync"
 else
-    echo "[NO] db has uncommited changes"
-    echo "SOLUTION:";
-    echo "./system-publish.sh"
-    exit;
+    echo "$(tput setaf 1)[NO]$(tput sgr 0) db has uncommited changes. SOLUTION: ./system-publish.sh"
 fi
 
 if [[ -z $(cd templates; git status -s) ]]; then
-    echo "[OK] record templates are in sync"
+    echo "$(tput setaf 2)[OK]$(tput sgr 0) record templates are in sync"
 else
-    echo "[NO] record templates have uncommited changes"
-    echo "SOLUTION:";
-    echo "./system-publish.sh"
-    exit;
+    echo "$(tput setaf 1)[NO]$(tput sgr 0) record templates have uncommited changes. SOLUTION: ./system-publish.sh"
 fi
 
 ./bin/test-submodules/index.sh
+
+echo "$(tput setaf 2)[OK]$(tput sgr 0) test completed";
