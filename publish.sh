07e3fe23812512603a931820315989e45dd5efc7
catpea <corporate.miou@gmail.com>
2021-05-07 21:04:08 -0400

    New Database Revision

--- publish.sh
+++ publish.sh
@@ -8,8 +8,7 @@
 if test -d ./bin; then
   cd bin;
   git pull;
-  git add .;
-  git commit -m 'New Bin Revisions';
+  git add . && git commit -m 'New Bin Revisions';
   git push;
   cd -
 fi
@@ -17,8 +16,7 @@ fi
 if test -d ./templates; then
   cd templates;
   git pull;
-  git add .;
-  git commit -m 'New Template Revisions';
+  git add . && git commit -m 'New Template Revisions';
   git push;
   cd -
 fi
@@ -26,13 +24,11 @@ fi
 if test -d ./homepage; then
   cd homepage;
   git pull;
-  git add .;
-  git commit -m 'New Homepage Revision';
+  git add . && git commit -m 'New Homepage Revision';
   git push;
   cd -
 fi
 
 git pull;
-git add .;
-git commit -m 'New Database Revision';
+git add . && git commit -m 'New Database Revision';
 git push;
