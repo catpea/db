8877c389d112b40d74f9e9a766ed1316c1a1f950
catpea <corporate.miou@gmail.com>
2021-06-19 21:40:35 -0400

    Prep for early launch :shipit:

--- import.sh
+++ import.sh
@@ -2,8 +2,8 @@
 
 # This is an old script, and once this sytem is operational can be removed.
 
-
-bowel dump ../poetry/dist/server-object/furkies-purrkies.json --root-dir ../poetry --image-dir ../poetry/src/image/ --cover-images ../poetry/src/image/ --audio-dir ../poetry/src/audio/ --web-dir ../poetry/docs/ --dist-dir ../poetry/dist/
+echo DANGER: furkies-purkies imports are now disabled, use the new system from now on.
+# bowel dump ../poetry/dist/server-object/furkies-purrkies.json --root-dir ../poetry --image-dir ../poetry/src/image/ --cover-images ../poetry/src/image/ --audio-dir ../poetry/src/audio/ --web-dir ../poetry/docs/ --dist-dir ../poetry/dist/
 
 echo DANGER: warrior imports are now disabled becasue they reset the index.json stop using the older system for publishing warrior.
 # bowel dump ../warrior/dist/server-object/westland-warrior.json --root-dir ../warrior --dist-dir ../warrior/dist/ --web-dir ../warrior/docs/ --yaml-db ../warrior/db/
