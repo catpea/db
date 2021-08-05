dc16f251051b4d985ebae908e68a9c0a660e0d5f
catpea <corporate.miou@gmail.com>
2021-08-05 18:55:13 -0400

    New Root Database Revision

--- configuration.mjs
+++ configuration.mjs
@@ -134,13 +134,25 @@ export default {
       description: "Just another dang old Audio Book about wisdom and growing up.",
 
       tagline: 'Home of Furkies Purrkies and Westland Warrior',
-      alert: `Possible hosting disruptions/delays ahead, <a href="http://catpea.com">catpea.com</a> will always be the most reliable way to access the site. However, the site is now at 5.2GB (from a generic/uncompressed perspective), that is probably/possibly 200MB over github recommendations (and the way they measure things), there maybe publishing delays or disruptions. To resolve this issue I am testing a new hosting company netlify. Here is a temporary test address that may have more recent publications: <a href="https://catpea.netlify.app/">https://catpea.netlify.app/</a> Please note that the YouTube channel has thus far been a reliable source of most recent audio poetry: <a href="https://www.youtube.com/channel/UC6Og8hy_iEgZutvp0SQmNzQ">Cat Pea University on YouTube</a> If netlify works out then I'll point catpea.com to it instead of github, and update this message.`,
+      alertTitle: 'Possible Hosting Disruption Notice',
+      alert: `
+      Possible hosting disruptions/delays ahead, <a href="http://catpea.com">catpea.com</a> will always be the most reliable way to access the site.
+      The site is now at some 5.2GB (apparently, on my disk it only shows 2.6 GB), and <a href="https://www.youtube.com/watch?v=QyFcl_Fba-k">github pages</a> fail to publish sometimes.
+      <a href="https://docs.github.com/en/github/managing-large-files/working-with-large-files/what-is-my-disk-quota">Github recommends 1GB websites with a 5GB hard limit</a>.
+      To resolve this issue I am now testing a series of hosting companies, both as a primary host, and a mirror host.
+      Here are the temporary addresses that I am using in my testing:
+      <a href="https://catpea.netlify.app/">catpea.netlify.app</a> (via Netlify),
+      <a href="https://catpea.vercel.app/">catpea.vercel.app</a> (via Vercel),
+      <a href="https://catpea.onrender.com/">catpea.onrender.com</a> (via Render), and
+      <a href="https://catpea.pages.dev/">catpea.pages.dev</a> (via Cloudflare). I am basically just looking for failures/annoyances over time.
+      Please note that the <a href="https://www.youtube.com/channel/UC6Og8hy_iEgZutvp0SQmNzQ">YouTube Channel</a> has thus far been a reliable source of most recent audio poetry: <a href="https://www.youtube.com/channel/UC6Og8hy_iEgZutvp0SQmNzQ">Cat Pea University on YouTube</a>.
+      `,
       icon: "earbuds",
       order: "latest",
 
       // Social Network Links For Website Plugin
       links: [
-        
+
         {
           name: "Netlify Mirror",
           icon: "link-45deg",
